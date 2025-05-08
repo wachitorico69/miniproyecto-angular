a5d202c0997b255a76d6059dee0b19be7e91ca42
Minato <rickycamposa@gmail.com>
2025-05-07 19:41:38 -0600

    Home
    Se agrego un carrousel usando Angular Material

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  images: string[] = [
+    'assets/img1.jpg',
+    'assets/img2.jpg',
+    'assets/img3.jpg'
+  ];
+
+  currentIndex = 0;
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  }
+
+  prevImage() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.images.length) % this.images.length;
+  }
+}
