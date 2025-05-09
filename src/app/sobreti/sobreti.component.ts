1dcd0ec73bc941e8a63c61790a3a76463de277ee
wachitorico69 <morenodario919@gmail.com>
2025-05-09 14:05:00 -0600

    rename sesion

---
+++ src/app/sobreti/sobreti.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sobreti',
+  imports: [],
+  templateUrl: './sobreti.component.html',
+  styleUrl: './sobreti.component.css'
+})
+export class SobretiComponent {
+
+}
